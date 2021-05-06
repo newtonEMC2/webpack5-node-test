8cbc50983411b58c7a0f17a26feb3ff38ee9d226
alexiscg <alexis.corbacho@hotmail.com>
2021-05-06 19:59:40 +0200

    run changed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker stop nodejs-image-demooo
 docker rm $(docker ps -a -q)
 docker build -t webpack5test  .
-docker run --name nodejs-image-demooo -p 80:3002 -d webpack5test
+docker run --name nodejs-image-demooo -p 80:3002 --restart always -d webpack5test
